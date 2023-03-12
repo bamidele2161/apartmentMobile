c2f6bf74098a5dfd8cf035bab86fb1e47838411b
Akinyemi Bamidele <59539644+bamidele2161@users.noreply.github.com>
2023-03-12 09:08:14 +0100

    added splash screen

--- src/screens/Home/index.jsx
+++ src/screens/Home/index.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+import { View, Text } from "react-native";
 import { styles } from "./style";
 
 const Home = () => {
   return (
     <View style={styles.container}>
       <Text>Welcome Bamidele!</Text>
-      <StatusBar style="auto" />
+      {/* <StatusBar style="auto" /> */}
     </View>
   );
 };
